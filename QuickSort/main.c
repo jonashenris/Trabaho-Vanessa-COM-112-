4951c7ecc82a0ed3533c421b7bcd5fdbf777c11f
vitorpio <vitormarquespio@gmail.com>
2015-03-31 14:22:36 -0300

    Projeto base

---
+++ QuickSort/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: vitorpio
+ *
+ * Created on 26 de Março de 2015, 11:16
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
